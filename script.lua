e57affb6f67531c969a3f24be9a9def11dc4f4f6
TRFY3 <ere736444@gmail.com>
2026-05-23 16:03:29 +0300

    Initial commit: Add Kingdom World script

---
+++ script.lua
@@ -0,0 +1,8 @@
+-- Kingdom World Script
+-- This script is loaded via loadstring from the GitHub raw content URL
+
+print("Kingdom World Script Loaded!")
+
+-- Your script content here
+local scriptVersion = "1.0.0"
+print("Script Version: " .. scriptVersion)
